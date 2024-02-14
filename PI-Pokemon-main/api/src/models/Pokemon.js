fbc0d96de0330be35ed8176d3924fc8c05a4dcfe
Agus Kainer <aguskainer1998@outlook.com>
2024-02-14 09:49:54 -0300

    modelos creados y relaciones de tablas

--- PI-Pokemon-main/api/src/models/Pokemon.js
+++ PI-Pokemon-main/api/src/models/Pokemon.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/132.png",
+    },
+    hp: { type: DataTypes.INTEGER },
+    attack: {
+      type: DataTypes.INTEGER,
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
     },
   });
 };
